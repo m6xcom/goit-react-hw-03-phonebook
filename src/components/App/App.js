4097d82767c420b6cadef8d458eeac31d9865e60
m6xcom <65496114+m6xcom@users.noreply.github.com>
2021-03-13 12:09:49 +0200

    Adds structure of app

---
+++ src/components/App/App.js
@@ -0,0 +1,52 @@
+import React, { Component } from "react";
+import ContactForm from "../ContactForm/ContactForm";
+import Filter from "../Filter/Filter";
+import ContactsList from "../ContactsList/ContactsList";
+import "./App.css";
+
+class App extends Component {
+  state = {
+    contacts: [],
+    filter: "",
+  };
+  addContact = (contact) => {
+    if (this.state.contacts.every((el) => el.name !== contact.name)) {
+      this.setState((prevState) => {
+        return { contacts: [...prevState.contacts, contact] };
+      });
+    } else {
+      alert(`${contact.name} is already in contacts.`);
+    }
+  };
+  deleteContact = (contactId) => {
+    this.setState({
+      contacts: this.state.contacts.filter((el) => {
+        return el.id !== contactId;
+      }),
+    });
+  };
+  filterChange = (filter) => {
+    this.setState({ filter: filter });
+  };
+  filterContacts = () => {
+    return this.state.contacts.filter((el) => {
+      return el.name.toLowerCase().includes(this.state.filter);
+    });
+  };
+  render() {
+    return (
+      <div className="wrapper">
+        <h2>Phonebook</h2>
+        <ContactForm addContact={this.addContact} />
+        <h2 className="contactsTitle">Contacts</h2>
+        <Filter filterChange={this.filterChange} />
+        <ContactsList
+          contacts={this.filterContacts()}
+          deleteContact={this.deleteContact}
+        />
+      </div>
+    );
+  }
+}
+
+export default App;
